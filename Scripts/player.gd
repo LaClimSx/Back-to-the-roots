0372c71ef9b9a8c0a7a2c8d15ec562019f56d41a
Clément Josso <clement.josso@epfl.ch>
2024-10-16 15:06:35 +0200

    feat: inventory continues

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 var screen_size
 var last_dir = Vector2(0,0)
 
+@export var inventory : Inventory
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
