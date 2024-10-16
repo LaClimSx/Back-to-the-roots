553d328b35ef328a2fd2a145ef0a45ffb4a2f5ab
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2024-10-16 16:48:47 +0200

    the door is opening!!

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -9,3 +9,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_player_detected_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		$anim.play("opening")
+		await $anim.animation_finished
+		$anim.play("open")
+
+
+func _on_player_detected_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		$anim.play("closing")
+		await $anim.animation_finished
+		$anim.play("close")
