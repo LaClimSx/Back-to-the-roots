d98f8bbf9720add75699d81b89696e3ec31d2665
Clément Josso <clement.josso@epfl.ch>
2024-11-12 16:39:54 +0100

    feat: refactor tilemap into group of tilemap layers

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@onready var tile_map : TileMap = $TileMap
+@onready var ground : TileMapLayer = $LayerGroup/Ground
+@onready var crops : TileMapLayer = $LayerGroup/Crops
 @onready var carrot_item = preload("res://Inventory/Items/carrot.tres")
 @onready var inventory_gui : Control = $CanvasLayer/Inventory
 
@@ -32,7 +33,7 @@ func checkActions():
 	if Input.is_action_just_pressed("Interact"):
 		var player : Player = $Player
 		var selected_item : Item = inventory_gui.get_selected_item()
-		var player_tile_pos : Vector2i = tile_map.local_to_map(player.position)
+		var player_tile_pos : Vector2i = ground.local_to_map(player.position)
 		var viewing_tile : Vector2i
 		match player.direction:
 			0:
@@ -43,23 +44,23 @@ func checkActions():
 				viewing_tile = player_tile_pos + Vector2i(0,-1)
 			3:
 				viewing_tile = player_tile_pos + Vector2i(0,1)
-		viewing_tile.clamp(Vector2i.ZERO, tile_map.get_used_rect().size)
+		viewing_tile.clamp(Vector2i.ZERO, ground.get_used_rect().size)
 		print(viewing_tile)
 		
 		#If clicked tile has grown carrot, add carrot to inventory
-		if tile_map.get_cell_atlas_coords(1,viewing_tile) == Vector2i(4,1) :
+		if crops.get_cell_atlas_coords(viewing_tile) == Vector2i(4,1) :
 			$Player.inventory.insert(carrot_item)
 			print("Inserted")
 
 		#If player has hoe (not broken) and tile is dirt, plow the tile
-		if selected_item && selected_item.name  == "hoe" && selected_item.durability >= 1 && tile_map.get_cell_atlas_coords(0,viewing_tile) == Vector2i(10,4) :
-			tile_map.set_cell(0,viewing_tile,0,Vector2i(1,0))
+		if selected_item && selected_item.name  == "hoe" && selected_item.durability >= 1 && ground.get_cell_atlas_coords(viewing_tile) == Vector2i(10,4) :
+			ground.set_cell(viewing_tile,0,Vector2i(1,0))
 			inventory_gui.use_item()
 			print("Plowed")
 
 		#If player has carrot and tile is empty field, plant the carrot
-		if selected_item && selected_item.name == "carrot" && tile_map.get_cell_atlas_coords(0,viewing_tile) == Vector2i(2,0) && tile_map.get_cell_atlas_coords(1,viewing_tile) == Vector2i(-1,-1) :
-			tile_map.set_cell(1,viewing_tile,2,Vector2i(2,1))
+		if selected_item && selected_item.name == "carrot" && ground.get_cell_atlas_coords(viewing_tile) == Vector2i(2,0) && crops.get_cell_atlas_coords(viewing_tile) == Vector2i(-1,-1) :
+			crops.set_cell(viewing_tile,2,Vector2i(2,1))
 			inventory_gui.use_item()
 			print("Planted")
 
